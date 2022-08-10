c317509247354603e628f9a0f1b21c673207af75
Rodrigo Fernandez Moreno <rodrigofm@Rodrigos-MBP.fibertel.com.ar>
2022-08-10 14:09:57 -0300

    logica de cantidad de prods, precio total de prods y boton eliminar del carrito

--- js/carritoNuevo.js
+++ js/carritoNuevo.js
@@ -34,8 +34,8 @@ mostrarProductos();
 
 let carrito = [];
 
-function agregarProdAlCarrito(elemento){
-    let btn = elemento.target;
+function agregarProdAlCarrito(event){
+    let btn = event.target;
     let item = btn.closest('.card'); //obtener el contendor que tenga la clase mas cercana 'card'.
     let itemTitle = item.querySelector('.card-title').textContent; //para obtener el contenido de ese selector sin todo el html molestando.
     let itemPrice = item.querySelector('.card-text').textContent;
@@ -52,14 +52,21 @@ function agregarProdAlCarrito(elemento){
 }
 
 function pushItemAlCarrito(nuevoItem){
+
+    let inputCantidad = tableBody.getElementsByClassName('inputCantidad');
+
     //para que me sume otro a la cantidad en vez de agregarme el mismo otra vez.
-    for (let index = 0; index < carrito.length; index++) {
-        console.log(carrito[i].title);
+    for (let i = 0; i < carrito.length; i++) { //recorro el carrito.
+        if (carrito[i].title.trim() === nuevoItem.title.trim()){ //.trim() para que quite todos los espacios que esten a los lados y no me de false por un espacio. Si esto se cumple significa que ya esta agregado nuestro producto porque titlte es nuestro identificador.
+            carrito[i].cantidad++;
+            inputCantidad[i].value++ //con [i] indico la posicion donde nos encontramos ya que todos los prod agregados tienen el input de cantidad.
+            precioTotal();
+            return null; //para q no se ejecute carrito.push ni el renderizado. Va a salir de la function pricipal pushItemAlCarrito.
+        }                //hace que no me agregue devuelta el producto que elegi.
     }
 
     carrito.push(nuevoItem);
-    // alert(`Agregaste ${nuevoItem.title} al carrito.`)
-    console.log(carrito);
+
     renderizarCarrito();
 }
 
@@ -69,14 +76,46 @@ function renderizarCarrito() {
     tableBody.innerHTML = '';//cada vez que se ejecute esta accion ESTE VACIO. Para que no me agregue devuelta lo que ya habia agregado.
     carrito.map(elemento => {
         let divCarrito = document.createElement('tr'); //me fijo desde el bootstap como esta armado para agregarlo.
+        divCarrito.classList.add('itemCarritoContainer');
         divCarrito.innerHTML = `<th scope="row">1</th>
-            <td>${elemento.title}</td>
+            <td class="title">${elemento.title}</td>
             <td>${elemento.price}</td>
             <td>
-              <input type="number" min="1" value=${elemento.cantidad}>
-              <i class="bi bi-trash"></i>
+              <input type="number" min="1" value=${elemento.cantidad} class="inputCantidad">
+              <button class="btnBorrar"><i class="bi bi-trash"></i></button>
             </td>`
 
-       tableBody.appendChild(divCarrito);
+        tableBody.appendChild(divCarrito);
+
+        divCarrito.querySelector('.btnBorrar').addEventListener('click', eliminarProducto);
+
     })
+    precioTotal();
+}
+
+function precioTotal(){
+    let total = 0;
+    let totalCarrito = document.getElementById('totalCarrito');
+
+    carrito.forEach(item => {
+        let precio = Number(item.price.replace("$", ''));//porque el precio tiene el signo de pesos y lo transformo en un valor numerico.
+        total = total + precio*item.cantidad;
+    })
+
+    totalCarrito.innerHTML = `Total: $${total}`
+}
+
+function eliminarProducto(event){
+    let btnBorrar = event.target;
+    let tr = btnBorrar.closest(".itemCarritoContainer");//tr es el cont padre y antes le habia asinado la clase itemCarritoContainer
+    let identificador = tr.querySelector('.title').textContent;//mi identificador es el title entonces busco donde este la clase title y capturo su contenido para luego compararlo en el IF.
+
+    for (let i = 0; i < carrito.length; i++) {
+        if (carrito[i].title == identificador) {
+            carrito.splice(i, 1);// i es la posicion que quiero eliminar, y el 1 que quiero eliminar uno solo.
+        }
+        
+    }
+    tr.remove();
+    precioTotal(); //despues que se ejecuta el remove, que se ejecute la sumatoria del total otra vez (sino me eliminaba el prod pero el total anterior seguia estando).
 }
